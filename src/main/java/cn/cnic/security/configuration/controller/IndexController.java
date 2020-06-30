3310e2615c6d791a2070515a9d4b3d5adc36e461
chenkai <ck770701780@hotmail.com>
2020-06-30 18:28:12 +0800

    使用工具类中的IP获取方法

--- src/main/java/cn/cnic/security/configuration/controller/IndexController.java
+++ src/main/java/cn/cnic/security/configuration/controller/IndexController.java
@@ -3,7 +3,6 @@
  */
 package cn.cnic.security.configuration.controller;
 
-import java.net.InetAddress;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cn.cnic.security.common.utils.IPUtils;
 import cn.cnic.security.common.utils.R;
 import cn.cnic.security.configuration.entity.UserEntity;
 import cn.cnic.security.configuration.service.UserService;
@@ -45,7 +45,7 @@ public class IndexController {
 		String pw = user.getLocalPassword();
 		String pws = (String) params.get("password");
 		if(pw.equals(pws)) {
-			user.setLastIp(getIpAddress());
+			user.setLastIp(IPUtils.getIpAddr(request));
 			user.setLastTime(new Date());
 			user.setLoginNumber(user.getLoginNumber() == null ? 1 : user.getLoginNumber() + 1);
 			user.setAuthenticationNumber(user.getAuthenticationNumber() == null ? 1 : user.getAuthenticationNumber() + 1);
@@ -55,37 +55,6 @@ public class IndexController {
 		user.setAuthenticationNumber(user.getAuthenticationNumber() == null ? 1 : user.getAuthenticationNumber() + 1);
 		userService.updateById(user);
 		return R.error("密码错误");	    
-	}
-	
-	
-	private String getIpAddress() {
-		String ip = request.getHeader("x-forwarded-for");
-		if(ip == null || ip.length() == 0 || "unknow".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length () == 0 || "unknown".equalsIgnoreCase (ip)) {
-	        ip = request.getHeader ("WL-Proxy-Client-IP");
-	    }
-		 if (ip == null || ip.length () == 0 || "unknown".equalsIgnoreCase (ip)) {
-		        ip = request.getRemoteAddr ();
-		        if (ip.equals ("127.0.0.1")) {
-		            //根据网卡取本机配置的IP
-		            InetAddress inet = null;
-		            try {
-		                inet = InetAddress.getLocalHost ();
-		            } catch (Exception e) {
-		                e.printStackTrace ();
-		            }
-		            ip = inet.getHostAddress ();
-		        }
-		    }
-		    // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-		    if (ip != null && ip.length () > 15) {
-		        if (ip.indexOf (",") > 0) {
-		            ip = ip.substring (0, ip.indexOf (","));
-		        }
-		    }
-		    return ip;				
 	}
 	
 }
